b892d67bea4f0412ca32b83fec1dddf1355b64ee
syedshamail1 <syedshamail3@gmail.com>
2023-04-21 02:26:56 +0500

    add validator

---
+++ src/stories/Requirements.stories.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import  {Requirements}  from '../components/Requirements';
+import './Requirements.css';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+    title: 'Example/Requirements',
+    component: Requirements,
+    tags: ['autodocs'],
+  };
+
+  export const Primary = () => {
+  const [valid, setValid] = useState(false);
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const passwordRequirements = [
+    {
+      text: 'Must be at least 8 characters',
+      validator: val => val.length >= 8,
+    },
+    {
+      text: 'Must contain at least one number',
+      validator: val => /\d/g.test(val),
+    },
+    {
+      text: 'Must contain at least one lower-case letter',
+      validator: val => /[a-z]/g.test(val),
+    },
+    {
+      text: 'Must contain at least one upper-case letter',
+      validator: val => /[A-Z]/g.test(val),
+    }
+  ];
+
+  return (
+    <div className='form'>
+      <h1>Signup</h1>
+
+      <Requirements
+        value={password}
+        requirements={passwordRequirements}
+        onValidChange={isValid => setValid(isValid)}
+      />
+
+      <input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+      <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+      <button disabled={!valid || !username}>Sign Up</button>
+    </div>
+  );
+}
